c8fb72f953a9a7e9d4b9d6ee0b2254e9052b7535
Tejas Retharekar <tejas.retharekar@gmail.com>
2026-01-26 16:19:06 +0530

    name added in contacts

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -88,10 +88,10 @@ const Contact = () => {
                                     <h3 className="text-2xl font-bold mb-4">Call Us</h3>
                                     <div className="flex flex-col space-y-3 text-lg font-medium text-teal-50">
                                         <a href="tel:+919028636288" className="hover:text-white hover:translate-x-1 transition-all">
-                                            +91 90286 36288
+                                            Shubham: +91 90286 36288
                                         </a>
                                         <a href="tel:+917620334946" className="hover:text-white hover:translate-x-1 transition-all">
-                                            +91 76203 34946
+                                            Krishna: +91 76203 34946
                                         </a>
                                     </div>
                                 </div>
